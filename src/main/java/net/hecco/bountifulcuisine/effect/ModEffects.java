96c2dfa37f5b269140950a0f0b466b2d85f387d5
Hecco <126524389+Hecco56@users.noreply.github.com>
2023-11-12 23:34:19 -0600

    Update ModEffects.java

--- src/main/java/net/hecco/bountifulcuisine/effect/ModEffects.java
+++ src/main/java/net/hecco/bountifulcuisine/effect/ModEffects.java
@@ -1,6 +1,8 @@
 package net.hecco.bountifulcuisine.effect;
 
 import net.hecco.bountifulcuisine.BountifulCuisine;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.registry.Registries;
@@ -11,7 +13,7 @@ public class ModEffects {
     public static final StatusEffect ACIDIC = registerStatusEffect("acidic", new AcidicEffect(StatusEffectCategory.NEUTRAL, 0xE7FF6D));
     public static final StatusEffect STUPOR = registerStatusEffect("stupor", new StuporEffect(StatusEffectCategory.NEUTRAL, 0x4539B3));
     public static final StatusEffect EBULLIENCE = registerStatusEffect("ebullience", new EbullienceEffect(StatusEffectCategory.BENEFICIAL, 0xE9DEE2));
-    public static final StatusEffect GORGING = registerStatusEffect("gorging", new GorgingEffect(StatusEffectCategory.NEUTRAL, 0xFFCC00));
+    public static final StatusEffect GORGING = registerStatusEffect("gorging", new GorgingEffect(StatusEffectCategory.NEUTRAL, 0xFFCC00).addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED, "91AEAA56-376B-4498-935B-2F7F68070635", 0.13, EntityAttributeModifier.Operation.MULTIPLY_TOTAL));
     private static StatusEffect registerStatusEffect(String name, StatusEffect statusEffect) {
         return Registry.register(Registries.STATUS_EFFECT, new Identifier(BountifulCuisine.MOD_ID, name), statusEffect);
     }
