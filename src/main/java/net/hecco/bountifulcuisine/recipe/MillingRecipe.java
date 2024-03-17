df930cb5a7764993e1a442817e3cd6fadbe198f8
Hecco <126524389+Hecco56@users.noreply.github.com>
2024-03-17 15:39:59 -0500

    woops no datagen

--- src/main/java/net/hecco/bountifulcuisine/recipe/MillingRecipe.java
+++ src/main/java/net/hecco/bountifulcuisine/recipe/MillingRecipe.java
@@ -1,7 +1,5 @@
 package net.hecco.bountifulcuisine.recipe;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
@@ -10,8 +8,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.recipe.*;
 import net.minecraft.registry.DynamicRegistryManager;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.JsonHelper;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.dynamic.Codecs;
 import net.minecraft.world.World;
