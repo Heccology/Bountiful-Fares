2ca700d584ccedda13383f6d381cebac3065695d
Hecco <126524389+Hecco56@users.noreply.github.com>
2024-03-30 13:33:04 -0500

    fixed sniffer digging loot tables

--- src/main/java/net/hecco/bountifulfares/util/ModLootTableModifiers.java
+++ src/main/java/net/hecco/bountifulfares/util/ModLootTableModifiers.java
@@ -1,10 +1,12 @@
 package net.hecco.bountifulfares.util;
 
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.hecco.bountifulfares.block.ModBlocks;
 import net.hecco.bountifulfares.item.ModItems;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
+import net.minecraft.loot.LootTables;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.EmptyEntry;
 import net.minecraft.loot.entry.ItemEntry;
@@ -24,16 +26,10 @@ public class ModLootTableModifiers {
     private static final Identifier ELDER_GUARDIAN_ID = new Identifier("minecraft", "entities/elder_guardian");
     private static final Identifier GUARDIAN_ID = new Identifier("minecraft", "entities/guardian");
     public static void modifyLootTables() {
-        LootTableEvents.REPLACE.register((resourceManager, lootManager, id, original, source) -> {
-//            if(SNIFFER_DIGGING_ID.equals(id)) {
-//                List<LootPoolEntry> entries = new ArrayList<>(Arrays.asList(original.pools[0].entries));
-//                entries.add(ItemEntry.builder(ModItems.LAPISBERRY_SEEDS).build());
-//                entries.add(ItemEntry.builder(ModItems.HOARY_SEEDS).build());
-//                LootPool.Builder pool = LootPool.builder().with(entries);
-//                return LootTable.builder().pool(pool).build();
-//            }
-
-            return null;
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if (source.isBuiltin() && SNIFFER_DIGGING_ID.equals(id)) {
+                tableBuilder.modifyPools(builder -> builder.with(ItemEntry.builder(ModItems.LAPISBERRY_SEEDS)).with(ItemEntry.builder(ModItems.HOARY_SEEDS)));
+            }
         });
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
             if (source.isBuiltin() && ELDER_GUARDIAN_ID.equals(id)) {
