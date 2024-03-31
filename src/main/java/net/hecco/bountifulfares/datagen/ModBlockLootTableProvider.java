26f0858104521a93b09274814af422b4ea58e5a8
Hecco <126524389+Hecco56@users.noreply.github.com>
2024-03-30 19:24:51 -0500

    fermentation vessel fix

--- src/main/java/net/hecco/bountifulfares/datagen/ModBlockLootTableProvider.java
+++ src/main/java/net/hecco/bountifulfares/datagen/ModBlockLootTableProvider.java
@@ -173,6 +173,7 @@ public class ModBlockLootTableProvider extends FabricBlockLootTableProvider {
         addDrop(ModBlocks.CERAMIC_CLAY_BLOCK);
         addDrop(ModBlocks.CERAMIC_PRESSURE_PLATE);
         addDrop(ModBlocks.CERAMIC_BUTTON);
+        addDrop(ModBlocks.FERMENTATION_VESSEL);
         addDrop(ModBlocks.GRISTMILL);
         addDrop(ModBlocks.HONEYSUCKLE);
         addDrop(ModBlocks.VIOLET_BELLFLOWER);
