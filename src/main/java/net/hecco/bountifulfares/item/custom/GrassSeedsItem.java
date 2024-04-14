f7c5515bcb09fd6508faedefa2d14a504c5ff7ad
Hecco <126524389+Hecco56@users.noreply.github.com>
2024-04-14 15:49:27 -0500

    Update GrassSeedsItem.java

--- src/main/java/net/hecco/bountifulfares/item/custom/GrassSeedsItem.java
+++ src/main/java/net/hecco/bountifulfares/item/custom/GrassSeedsItem.java
@@ -1,10 +1,13 @@
 package net.hecco.bountifulfares.item.custom;
 
+import net.hecco.bountifulfares.block.ModBlocks;
 import net.hecco.bountifulfares.util.ModBlockTags;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BoneMealItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.particle.BlockStateParticleEffect;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
@@ -23,7 +26,9 @@ public class GrassSeedsItem extends Item {
         if (world.getBlockState(pos).isIn(ModBlockTags.GRASS_SEEDS_PLANTABLE_ON) && world.getBlockState(pos.up()).isAir()) {
             world.setBlockState(pos, Blocks.GRASS_BLOCK.getDefaultState());
             world.playSound(null, pos, SoundEvents.ITEM_CROP_PLANT, SoundCategory.BLOCKS, 1.0f, 0.8f + world.random.nextFloat() * 0.4f);
-            BoneMealItem.createParticles(world, pos, 0);
+            for (int i = 0; i < 16; i++) {
+                world.addParticle(ParticleTypes.HAPPY_VILLAGER, (pos.getX() - 0.2) + (world.random.nextFloat() * 1.4), pos.getY() + (world.random.nextFloat() * 0.5) + 0.8, (pos.getZ() - 0.2) + (world.random.nextFloat() * 1.4), (world.random.nextFloat() - 0.5) / 8, (world.random.nextFloat() - 0.5) / 8, (world.random.nextFloat() - 0.5) / 8);
+            }
             return ActionResult.success(true);
         }
         return super.useOnBlock(context);
