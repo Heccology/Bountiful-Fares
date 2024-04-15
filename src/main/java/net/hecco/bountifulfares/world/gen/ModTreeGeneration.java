4e8359b754ddea3d742c91af936ca7e715c63c00
Hecco <126524389+Hecco56@users.noreply.github.com>
2024-04-15 17:19:36 -0500

    custom biome tags for generation

--- src/main/java/net/hecco/bountifulfares/world/gen/ModTreeGeneration.java
+++ src/main/java/net/hecco/bountifulfares/world/gen/ModTreeGeneration.java
@@ -3,6 +3,7 @@ package net.hecco.bountifulfares.world.gen;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.hecco.bountifulfares.BountifulFares;
+import net.hecco.bountifulfares.util.ModBiomeTags;
 import net.hecco.bountifulfares.world.ModPlacedFeatures;
 import net.minecraft.registry.tag.BiomeTags;
 import net.minecraft.world.biome.BiomeKeys;
@@ -11,15 +12,15 @@ import net.minecraft.world.gen.GenerationStep;
 public class ModTreeGeneration {
     public static void generateTrees() {
         if (BountifulFares.CONFIG.isGenerateAppleTrees()) {
-            BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.FOREST),
+            BiomeModifications.addFeature(BiomeSelectors.tag(ModBiomeTags.HAS_APPLE_TREES),
                     GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.APPLE_PLACED_KEY);
         }
         if (BountifulFares.CONFIG.isGenerateOrangeTrees()) {
-            BiomeModifications.addFeature(BiomeSelectors.tag(BiomeTags.IS_JUNGLE),
+            BiomeModifications.addFeature(BiomeSelectors.tag(ModBiomeTags.HAS_ORANGE_TREES),
                     GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.ORANGE_PLACED_KEY);
         }
         if (BountifulFares.CONFIG.isGenerateLemonTrees()) {
-            BiomeModifications.addFeature(BiomeSelectors.tag(BiomeTags.IS_RIVER),
+            BiomeModifications.addFeature(BiomeSelectors.tag(ModBiomeTags.HAS_LEMON_TREES),
                     GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.LEMON_PLACED_KEY);
         }
         if (BountifulFares.CONFIG.isGeneratePlumTrees()) {
@@ -27,7 +28,7 @@ public class ModTreeGeneration {
                 BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.FLOWER_FOREST),
                         GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.PLUM_FLOWER_FOREST_PLACED_KEY);
             }
-            BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.MEADOW),
+            BiomeModifications.addFeature(BiomeSelectors.tag(ModBiomeTags.HAS_PLUM_TREES),
                     GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.PLUM_PLACED_KEY);
         }
         if (BountifulFares.CONFIG.isGenerateAllFruitTreesInFlowerForest()) {
@@ -40,13 +41,13 @@ public class ModTreeGeneration {
             BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.FLOWER_FOREST),
                     GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.PLUM_PLACED_KEY);
         }
-        BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.WINDSWEPT_FOREST, BiomeKeys.WOODED_BADLANDS),
+        BiomeModifications.addFeature(BiomeSelectors.tag(ModBiomeTags.HAS_WALNUT_TREES),
                 GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.WALNUT_PLACED_KEY);
         BiomeModifications.addFeature(BiomeSelectors.tag(BiomeTags.IS_BEACH),
                 GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.BEACH_WALNUT_PLACED_KEY);
-        BiomeModifications.addFeature(BiomeSelectors.tag(BiomeTags.IS_JUNGLE),
+        BiomeModifications.addFeature(BiomeSelectors.tag(ModBiomeTags.HAS_WILD_PASSION_FRUIT),
                 GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.PASSION_FRUIT_JUNGLE_TREE);
-        BiomeModifications.addFeature(BiomeSelectors.tag(BiomeTags.SWAMP_HUT_HAS_STRUCTURE),
+        BiomeModifications.addFeature(BiomeSelectors.tag(ModBiomeTags.HAS_WILD_ELDERBERRIES),
                 GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.ELDERBERRY_SWAMP_OAK);
         if (BountifulFares.CONFIG.isGenerateForestWalnutTrees()) {
             BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.FOREST),
