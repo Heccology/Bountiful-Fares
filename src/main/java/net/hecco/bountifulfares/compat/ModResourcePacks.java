9cc60e995714e846f5d19454c2284943e57106ca
Hecco <126524389+Hecco56@users.noreply.github.com>
2024-04-28 15:23:34 -0500

    fix compat for real this time

--- src/main/java/net/hecco/bountifulfares/compat/ModResourcePacks.java
+++ src/main/java/net/hecco/bountifulfares/compat/ModResourcePacks.java
@@ -12,36 +12,38 @@ import java.util.Optional;
 
 public class ModResourcePacks {
     private static void registerBuiltinResourcePack(ModContainer modContainer, String forModID) {
-        if (FabricLoader.getInstance().isModLoaded(forModID)) {
             ResourceManagerHelper.registerBuiltinResourcePack(
                     new Identifier(BountifulFares.MOD_ID, forModID + "_res"),
                     modContainer,
                     Text.translatable("pack." + BountifulFares.MOD_ID + "." + forModID),
                     ResourcePackActivationType.ALWAYS_ENABLED
             );
-        }
     }
 
-    private static void registerBuiltinDataPack(ModContainer modContainer, String packId, String nameId) {
-        if (!FabricLoader.getInstance().isModLoaded(packId)) {
+    private static void registerBuiltinDataPack(ModContainer modContainer, String packId) {
             ResourceManagerHelper.registerBuiltinResourcePack(
                     new Identifier(BountifulFares.MOD_ID, packId + "_dat"),
                     modContainer,
-                    Text.translatable("pack." + BountifulFares.MOD_ID + "." + nameId),
+                    Text.translatable("pack." + BountifulFares.MOD_ID + "." + packId),
                     ResourcePackActivationType.ALWAYS_ENABLED
             );
-        }
     }
 
     public static void registerBuiltinResourcePacks() {
         Optional<ModContainer> modContainer = FabricLoader.getInstance().getModContainer(BountifulFares.MOD_ID);
         if (modContainer.isPresent()) {
-            registerBuiltinResourcePack(modContainer.get(), BountifulFares.AMENDMENTS_MOD_ID);
-            registerBuiltinDataPack(modContainer.get(), BountifulFares.AMENDMENTS_MOD_ID, BountifulFares.AMENDMENTS_MOD_ID);
-            registerBuiltinDataPack(modContainer.get(), BountifulFares.ELS_AND_LS_DYES_MOD_ID, BountifulFares.DYE_DEPOT_MOD_ID);
-            registerBuiltinResourcePack(modContainer.get(), BountifulFares.DYE_DEPOT_MOD_ID);
-            registerBuiltinResourcePack(modContainer.get(), BountifulFares.ELS_AND_LS_DYES_MOD_ID);
-            registerBuiltinDataPack(modContainer.get(), BountifulFares.DYE_DEPOT_MOD_ID, BountifulFares.ELS_AND_LS_DYES_MOD_ID);
+            if (FabricLoader.getInstance().isModLoaded(BountifulFares.AMENDMENTS_MOD_ID)) {
+                registerBuiltinResourcePack(modContainer.get(), BountifulFares.AMENDMENTS_MOD_ID);
+                registerBuiltinDataPack(modContainer.get(), BountifulFares.AMENDMENTS_MOD_ID);
+            }
+            if (FabricLoader.getInstance().isModLoaded(BountifulFares.DYE_DEPOT_MOD_ID)) {
+                registerBuiltinResourcePack(modContainer.get(), BountifulFares.DYE_DEPOT_MOD_ID);
+                registerBuiltinDataPack(modContainer.get(), BountifulFares.DYE_DEPOT_MOD_ID);
+            }
+            if (FabricLoader.getInstance().isModLoaded(BountifulFares.ELS_AND_LS_DYES_MOD_ID)) {
+                registerBuiltinResourcePack(modContainer.get(), BountifulFares.ELS_AND_LS_DYES_MOD_ID);
+                registerBuiltinDataPack(modContainer.get(), BountifulFares.ELS_AND_LS_DYES_MOD_ID);
+            }
         }
     }
 }
