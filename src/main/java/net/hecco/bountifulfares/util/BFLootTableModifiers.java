deb8cd526de704d9f859a94203080e46fe20ac69
Hecco <126524389+Hecco56@users.noreply.github.com>
2024-06-01 17:35:34 -0500

    changed grass seeds loot table

--- src/main/java/net/hecco/bountifulfares/util/BFLootTableModifiers.java
+++ src/main/java/net/hecco/bountifulfares/util/BFLootTableModifiers.java
@@ -10,10 +10,50 @@ import net.minecraft.util.Identifier;
 
 public class BFLootTableModifiers {
 
+    private static final Identifier GRASS_ID = new Identifier("minecraft", "blocks/grass");
+    private static final Identifier TALL_GRASS_ID = new Identifier("minecraft", "blocks/tall_grass");
+    private static final Identifier FERN_ID = new Identifier("minecraft", "blocks/fern");
+    private static final Identifier LARGE_FERN_ID = new Identifier("minecraft", "blocks/large_fern");
     private static final Identifier SNIFFER_DIGGING_ID = new Identifier("minecraft", "gameplay/sniffer_digging");
     private static final Identifier ELDER_GUARDIAN_ID = new Identifier("minecraft", "entities/elder_guardian");
     private static final Identifier GUARDIAN_ID = new Identifier("minecraft", "entities/guardian");
     public static void modifyLootTables() {
+            LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+                if (source.isBuiltin() && GRASS_ID.equals(id)) {
+                    LootPool.Builder poolBuilder = LootPool.builder()
+                            .with(ItemEntry.builder(BFItems.GRASS_SEEDS).weight(1))
+                            .with(EmptyEntry.builder().weight(5));
+
+                    tableBuilder.pool(poolBuilder);
+                }
+            });
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if (source.isBuiltin() && TALL_GRASS_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .with(ItemEntry.builder(BFItems.GRASS_SEEDS).weight(1))
+                        .with(EmptyEntry.builder().weight(5));
+
+                tableBuilder.pool(poolBuilder);
+            }
+        });
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if (source.isBuiltin() && FERN_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .with(ItemEntry.builder(BFItems.GRASS_SEEDS).weight(1))
+                        .with(EmptyEntry.builder().weight(5));
+
+                tableBuilder.pool(poolBuilder);
+            }
+        });
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if (source.isBuiltin() && LARGE_FERN_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .with(ItemEntry.builder(BFItems.GRASS_SEEDS).weight(1))
+                        .with(EmptyEntry.builder().weight(5));
+
+                tableBuilder.pool(poolBuilder);
+            }
+        });
         if (BountifulFares.CONFIG.isEnableLapisberrySeeds()) {
             LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
                 if (source.isBuiltin() && SNIFFER_DIGGING_ID.equals(id)) {
