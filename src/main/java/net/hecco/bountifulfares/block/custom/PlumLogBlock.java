864107e2103c10ff3689e31bd372811fcb9f9e78
Hecco <126524389+Hecco56@users.noreply.github.com>
2024-06-06 19:39:26 -0500

    cleaned up fruit log code

--- src/main/java/net/hecco/bountifulfares/block/custom/PlumLogBlock.java
+++ src/main/java/net/hecco/bountifulfares/block/custom/PlumLogBlock.java
@@ -9,45 +9,18 @@ import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
 
 public class PlumLogBlock extends FruitLogBlock {
-    public PlumLogBlock(Block leaves, Block floweringleaves, Settings settings) {
-        super(leaves, floweringleaves, settings);
+    public PlumLogBlock(Settings settings) {
+        super(settings);
     }
 
     @Override
-    protected boolean shouldBeLeafy(WorldView world, BlockPos pos, BlockState blockState) {
-        Direction.Axis axis = blockState.get(PlumLogBlock.AXIS);
-        Direction[] directions;
-        if (axis == Direction.Axis.X) {
-            directions = new Direction[]{Direction.UP, Direction.DOWN, Direction.NORTH, Direction.SOUTH};
-        } else if (axis == Direction.Axis.Z) {
-            directions = new Direction[]{Direction.UP, Direction.DOWN, Direction.WEST, Direction.EAST};
-        } else {
-            // No rotation needed for Y axis
-            directions = new Direction[]{Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST};
-        }
-
-        for (Direction direction : directions) {
-            BlockState neighborState = world.getBlockState(pos.offset(direction));
-            if (neighborState.getBlock() != BFBlocks.PLUM_LEAVES && neighborState.getBlock() != BFBlocks.FLOWERING_PLUM_LEAVES) {
-                return false;
-            }
-        }
-
-        return true;
+    public Block getLeavesBlock() {
+        return BFBlocks.PLUM_LEAVES;
     }
 
     @Override
-    public void neighborUpdate(BlockState state, World world, BlockPos pos, Block block, BlockPos fromPos, boolean notify) {
-        if (!world.isClient) {
-            boolean shouldBeLeafy = shouldBeLeafy(world, pos, state);
-            if (shouldBeLeafy && !state.get(LEAFY)) {
-                world.setBlockState(pos, state.with(LEAFY, true), 2);
-                world.updateListeners(pos, state, state, 3); // Trigger immediate block update
-            } else if (!shouldBeLeafy && state.get(LEAFY)) {
-                world.setBlockState(pos, state.with(LEAFY, false), 2);
-                world.updateListeners(pos, state, state, 3); // Trigger immediate block update
-            }
-        }
+    public Block getFloweringLeavesBlock() {
+        return BFBlocks.FLOWERING_PLUM_LEAVES;
     }
 
 }
