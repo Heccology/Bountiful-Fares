1eef8895bc57d80f8614f35217110c2ffae89e5b
Hecco <126524389+Hecco56@users.noreply.github.com>
2024-06-17 09:47:23 -0500

    update palm sapling placement

--- src/main/java/net/hecco/bountifulfares/block/custom/PalmSaplingBlock.java
+++ src/main/java/net/hecco/bountifulfares/block/custom/PalmSaplingBlock.java
@@ -1,18 +1,24 @@
 package net.hecco.bountifulfares.block.custom;
 
-import net.hecco.bountifulfares.BountifulFares;
+import net.hecco.bountifulfares.block.BFBlocks;
 import net.hecco.bountifulfares.util.BFBlockTags;
 import net.minecraft.block.*;
 import net.minecraft.block.sapling.SaplingGenerator;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
+import org.jetbrains.annotations.Nullable;
 
 public class PalmSaplingBlock extends SaplingBlock {
+    public static final BooleanProperty NATURAL = BooleanProperty.of("natural");
     public PalmSaplingBlock(SaplingGenerator generator, Settings settings) {
         super(generator, settings);
+        this.setDefaultState(this.getStateManager().getDefaultState().with(NATURAL, true));
     }
 
     @Override
@@ -21,17 +27,36 @@ public class PalmSaplingBlock extends SaplingBlock {
     }
 
     @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        super.appendProperties(builder);
+        builder.add(NATURAL);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        if (ctx.getPlayer() != null) {
+            return BFBlocks.PALM_SAPLING.getDefaultState().with(NATURAL, false);
+        }
+        return super.getPlacementState(ctx);
+    }
+
+    @Override
     public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
         if (this.canPlantOnTop(world.getBlockState(pos.down()), world, pos)) {
-            BlockPos blockPos = pos.down();
-            for (int k = 0; k < 2; k++) {
-                for (int i = 0; i < 17; i++) {
-                    for (int j = 0; j < 17; j++) {
-                        if (world.getBlockState(blockPos.add(i - 8, -k, j - 8)).isOf(Blocks.WATER)) {
-                            return true;
+            if (state.get(NATURAL)) {
+                BlockPos blockPos = pos.down();
+                for (int k = 0; k < 2; k++) {
+                    for (int i = 0; i < 11; i++) {
+                        for (int j = 0; j < 11; j++) {
+                            if (world.getBlockState(blockPos.add(i - 5, -k, j - 5)).getFluidState().isOf(Fluids.WATER)) {
+                                return true;
+                            }
                         }
                     }
                 }
+            } else {
+                return true;
             }
         }
         return false;
@@ -39,6 +64,6 @@ public class PalmSaplingBlock extends SaplingBlock {
 
     @Override
     protected boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
-        return floor.isIn(BFBlockTags.PALM_SAPLINGS_PLANTABLE_ON) || floor.isOf(Blocks.FARMLAND);
+        return floor.isIn(BFBlockTags.PALM_SAPLINGS_PLANTABLE_ON) || super.canPlantOnTop(floor, world, pos);
     }
 }
---
+++ src/main/java/net/hecco/bountifulfares/item/custom/CoconutItem.java
@@ -0,0 +1,10 @@
+package net.hecco.bountifulfares.item.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+
+public class CoconutItem extends BlockItem {
+    public CoconutItem(Block block, Settings settings) {
+        super(block, settings);
+    }
+}
