24c04287cec393e360cd3784be6bb06e6a3919d4
Hecco <126524389+Hecco56@users.noreply.github.com>
2024-06-17 09:16:30 -0500

    palm frond stuff

--- src/main/java/net/hecco/bountifulfares/block/custom/PalmSaplingBlock.java
+++ src/main/java/net/hecco/bountifulfares/block/custom/PalmSaplingBlock.java
@@ -1,10 +1,13 @@
 package net.hecco.bountifulfares.block.custom;
 
+import net.hecco.bountifulfares.BountifulFares;
 import net.hecco.bountifulfares.util.BFBlockTags;
 import net.minecraft.block.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 
 public class PalmSaplingBlock extends SaplingBlock {
     public PalmSaplingBlock(SaplingGenerator generator, Settings settings) {
@@ -14,6 +17,23 @@ public class PalmSaplingBlock extends SaplingBlock {
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         return Block.createCuboidShape(4, 0, 4, 12, 4, 12);
+    }
+
+    @Override
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        if (this.canPlantOnTop(world.getBlockState(pos.down()), world, pos)) {
+            BlockPos blockPos = pos.down();
+            for (int k = 0; k < 2; k++) {
+                for (int i = 0; i < 17; i++) {
+                    for (int j = 0; j < 17; j++) {
+                        if (world.getBlockState(blockPos.add(i - 8, -k, j - 8)).isOf(Blocks.WATER)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     @Override
