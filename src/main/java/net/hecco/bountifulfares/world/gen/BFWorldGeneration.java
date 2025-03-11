2a3c99255156c6084544488a1ff453dae3eaa2e7
Hecco <126524389+Hecco56@users.noreply.github.com>
2025-03-11 17:34:34 -0500

    remove everything

--- src/main/java/net/hecco/bountifulfares/world/gen/BFWorldGeneration.java
+++
@@ -1,9 +0,0 @@
-package net.hecco.bountifulfares.world.gen;
-
-public class BFWorldGeneration {
-
-    public static void generateModWorldGeneration() {
-        BFTreeGeneration.generateTrees();
-        BFFoliageGeneration.generateFlowers();
-    }
-}
