450fd12ef6c13ea2ebeb87344e79dbf57824639e
Hecco <126524389+Hecco56@users.noreply.github.com>
2025-03-26 16:41:48 -0500

    fix maize crash

--- src/main/java/net/hecco/bountifulfares/block/custom/MaizeCropBlock.java
+++ src/main/java/net/hecco/bountifulfares/block/custom/MaizeCropBlock.java
@@ -128,10 +128,51 @@ public class MaizeCropBlock extends CropBlock implements Fertilizable {
 
     }
 
+    protected static float getAvailableMoisture(Block block, BlockView world, BlockPos pos) {
+        float f = 1.0F;
+        BlockPos blockPos = pos.down();
+
+        for(int i = -1; i <= 1; ++i) {
+            for(int j = -1; j <= 1; ++j) {
+                float g = 0.0F;
+                BlockState blockState = world.getBlockState(blockPos.add(i, 0, j));
+                if (blockState.isOf(Blocks.FARMLAND)) {
+                    g = 1.0F;
+                    if (blockState.get(FarmlandBlock.MOISTURE) > 0) {
+                        g = 3.0F;
+                    }
+                }
+
+                if (i != 0 || j != 0) {
+                    g /= 4.0F;
+                }
+
+                f += g;
+            }
+        }
+
+        BlockPos blockPos2 = pos.north();
+        BlockPos blockPos3 = pos.south();
+        BlockPos blockPos4 = pos.west();
+        BlockPos blockPos5 = pos.east();
+        boolean bl = world.getBlockState(blockPos4).isOf(block) || world.getBlockState(blockPos5).isOf(block);
+        boolean bl2 = world.getBlockState(blockPos2).isOf(block) || world.getBlockState(blockPos3).isOf(block);
+        if (bl && bl2) {
+            f /= 2.0F;
+        } else {
+            boolean bl3 = world.getBlockState(blockPos4.north()).isOf(block) || world.getBlockState(blockPos5.north()).isOf(block) || world.getBlockState(blockPos5.south()).isOf(block) || world.getBlockState(blockPos4.south()).isOf(block);
+            if (bl3) {
+                f /= 2.0F;
+            }
+        }
+
+        return f;
+    }
+
     @Override
     public void randomTick(BlockState state, ServerWorld world, BlockPos pos, net.minecraft.util.math.random.Random random) {
         if (world.getBaseLightLevel(pos, 0) >= 9) {
-            float f = CropBlock.getAvailableMoisture(this, world, pos);
+            float f = getAvailableMoisture(this, world, pos);
             boolean bl = random.nextInt((int) (25.0F / f) + 1) == 0;
             if (bl) {
                 this.tryGrow(world, state, pos);
