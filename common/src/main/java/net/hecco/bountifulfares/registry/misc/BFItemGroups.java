b73bfa3b8b724afdb4a2a065505c681c7f6b24e5
Hecco <126524389+Hecco56@users.noreply.github.com>
2025-07-30 07:55:51 -0500

    paintings in tab

--- common/src/main/java/net/hecco/bountifulfares/registry/misc/BFItemGroups.java
+++ common/src/main/java/net/hecco/bountifulfares/registry/misc/BFItemGroups.java
@@ -6,29 +6,37 @@ import net.hecco.bountifulfares.compat.appledog.AppledogBlocks;
 import net.hecco.bountifulfares.compat.arts_and_crafts.ArtsAndCraftsBlocks;
 import net.hecco.bountifulfares.compat.excessive_building.ExcessiveBuildingBlocks;
 import net.hecco.bountifulfares.compat.natures_spirit.NaturesSpiritBlocks;
+import net.hecco.bountifulfares.item.component.TiffinContents;
 import net.hecco.bountifulfares.registry.content.BFBlocks;
+import net.hecco.bountifulfares.registry.content.BFComponents;
 import net.hecco.bountifulfares.registry.content.BFItems;
 import net.hecco.bountifulfares.registry.content.BFPotions;
 import net.hecco.heccolib.platform.HLServices;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.RegistryOps;
+import net.minecraft.world.entity.decoration.Painting;
 import net.minecraft.world.entity.decoration.PaintingVariant;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.alchemy.PotionContents;
+import net.minecraft.world.item.component.CustomData;
 import net.minecraft.world.level.block.Block;
 
 import java.util.Comparator;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class BFItemGroups {
-
-    private static final Comparator<Holder<PaintingVariant>> PAINTING_VARIANT_COMPARATOR = Comparator.comparing(Holder::value, Comparator.comparingInt((paintingVariant) -> 16 * 16));
     
     public static final Supplier<CreativeModeTab> BOUNTIFUL_FARES = HLServices.REGISTRY.register(
             BountifulFares.MOD_ID, "bountiful_fares", BuiltInRegistries.CREATIVE_MODE_TAB.key(), () -> CreativeModeTab.builder(CreativeModeTab.Row.TOP, 0)
@@ -406,23 +414,23 @@ public class BFItemGroups {
 //                        entries.accept(PotionContents.createItemStack(Items.LINGERING_POTION, BFPotions.LONG_STUPOR));
 //                        entries.accept(PotionContents.createItemStack(Items.TIPPED_ARROW, BFPotions.STUPOR));
 //                        entries.accept(PotionContents.createItemStack(Items.TIPPED_ARROW, BFPotions.LONG_STUPOR)); //TODO: FIX
-                        entries.accept(BFItems.TIFFINS.get(null).get());
-                        entries.accept(BFItems.TIFFINS.get(DyeColor.WHITE).get());
-                        entries.accept(BFItems.TIFFINS.get(DyeColor.LIGHT_GRAY).get());
-                        entries.accept(BFItems.TIFFINS.get(DyeColor.GRAY).get());
-                        entries.accept(BFItems.TIFFINS.get(DyeColor.BLACK).get());
-                        entries.accept(BFItems.TIFFINS.get(DyeColor.BROWN).get());
-                        entries.accept(BFItems.TIFFINS.get(DyeColor.RED).get());
-                        entries.accept(BFItems.TIFFINS.get(DyeColor.ORANGE).get());
-                        entries.accept(BFItems.TIFFINS.get(DyeColor.YELLOW).get());
-                        entries.accept(BFItems.TIFFINS.get(DyeColor.LIME).get());
-                        entries.accept(BFItems.TIFFINS.get(DyeColor.GREEN).get());
-                        entries.accept(BFItems.TIFFINS.get(DyeColor.CYAN).get());
-                        entries.accept(BFItems.TIFFINS.get(DyeColor.LIGHT_BLUE).get());
-                        entries.accept(BFItems.TIFFINS.get(DyeColor.BLUE).get());
-                        entries.accept(BFItems.TIFFINS.get(DyeColor.PURPLE).get());
-                        entries.accept(BFItems.TIFFINS.get(DyeColor.MAGENTA).get());
-                        entries.accept(BFItems.TIFFINS.get(DyeColor.PINK).get());
+                        addTiffin(null, entries);
+                        addTiffin(DyeColor.WHITE, entries);
+                        addTiffin(DyeColor.LIGHT_GRAY, entries);
+                        addTiffin(DyeColor.GRAY, entries);
+                        addTiffin(DyeColor.BLACK, entries);
+                        addTiffin(DyeColor.BROWN, entries);
+                        addTiffin(DyeColor.RED, entries);
+                        addTiffin(DyeColor.ORANGE, entries);
+                        addTiffin(DyeColor.YELLOW, entries);
+                        addTiffin(DyeColor.LIME, entries);
+                        addTiffin(DyeColor.GREEN, entries);
+                        addTiffin(DyeColor.CYAN, entries);
+                        addTiffin(DyeColor.LIGHT_BLUE, entries);
+                        addTiffin(DyeColor.BLUE, entries);
+                        addTiffin(DyeColor.PURPLE, entries);
+                        addTiffin(DyeColor.MAGENTA, entries);
+                        addTiffin(DyeColor.PINK, entries);
                         entries.accept(BFBlocks.ARTISAN_BREAD.get());
                         entries.accept(BFItems.ARTISAN_COOKIE.get());
                         entries.accept(BFBlocks.APPLE_PIE.get());
@@ -480,12 +488,17 @@ public class BFItemGroups {
                         entries.accept(BFItems.CANDIED_PLUM.get());
                         entries.accept(BFItems.CANDIED_ORANGE.get());
                         entries.accept(BFItems.CANDIED_LEMON.get());
-//                        displayContext.lookup().getOptionalWrapper(RegistryKeys.PAINTING_VARIANT).ifPresent((wrapper) -> {
-//                            addPaintings(entries, wrapper, (registryEntry) -> registryEntry.isIn(BFBlockTags.PAINTINGS), ItemGroup.StackVisibility.PARENT_AND_SEARCH_TABS.get());
-//                        }.get());
-//                        displayContext.holders().lookup(Registries.PAINTING_VARIANT).ifPresent((registryWrapper) -> {
-//                            addPaintings(entries, displayContext.holders(), registryWrapper, (registryEntry) -> registryEntry.is(BFBlockTags.PAINTINGS), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS.get());
-//                        }.get());
+
+                        addPainting(entries, "bountifulfares:aquaculture");
+                        addPainting(entries, "bountifulfares:citrus_dish");
+                        addPainting(entries, "bountifulfares:escalade");
+                        addPainting(entries, "bountifulfares:hazel_floret");
+                        addPainting(entries, "bountifulfares:phylogenesis");
+                        addPainting(entries, "bountifulfares:ruminer");
+                        addPainting(entries, "bountifulfares:unpleasant_tiles");
+                        addPainting(entries, "bountifulfares:violet_floret");
+                        addPainting(entries, "bountifulfares:why_blue");
+
 //                        entries.accept(AppledogBlocks.APPLEDOG_BLOCK.get());
 
                     }).build());
@@ -494,15 +507,17 @@ public class BFItemGroups {
 
     }
 
-//    private static void addPaintings(CreativeModeTab.Output entries, HolderLookup.Provider registryLookup, HolderLookup.RegistryLookup<PaintingVariant> registryWrapper, Predicate<Holder<PaintingVariant>> filter, CreativeModeTab.TabVisibility stackVisibility) {
-//        RegistryOps<Tag> registryOps = registryLookup.createSerializationContext(NbtOps.INSTANCE.get());
-//        registryWrapper.listElements().filter(filter).sorted(PAINTING_VARIANT_COMPARATOR).forEach((paintingVariantEntry) -> {
-//            CustomData nbtComponent = CustomData.EMPTY.update(registryOps, Painting.VARIANT_MAP_CODEC, paintingVariantEntry).getOrThrow().update((nbt) -> {
-//                nbt.putString("id", "minecraft:painting".get());
-//            }.get());
-//            ItemStack itemStack = new ItemStack(Items.PAINTING.get());
-//            itemStack.set(DataComponents.ENTITY_DATA, nbtComponent.get());
-//            entries.accept(itemStack, stackVisibility.get());
-//        }.get());
-//    }
+
+    private static void addTiffin(DyeColor color, CreativeModeTab.Output entries) {
+        ItemStack stack = BFItems.TIFFINS.get(color).get().getDefaultInstance();
+        stack.set(BFComponents.TIFFIN_CONTENTS.get(), new TiffinContents(Items.AIR, 0));
+        entries.accept(stack);
+    }
+
+    private static void addPainting(CreativeModeTab.Output entries, String id) {
+        CustomData nbtComponent = CustomData.EMPTY.update((nbt) -> nbt.putString("id", "minecraft:painting")).update((nbt) -> nbt.putString("variant", id));
+        ItemStack itemStack = new ItemStack(Items.PAINTING);
+        itemStack.set(DataComponents.ENTITY_DATA, nbtComponent);
+        entries.accept(itemStack);
+    }
 }
