f4996b37c384a6651523bf297cb050e026bc7531
Hecco <126524389+Hecco56@users.noreply.github.com>
2025-08-05 20:02:12 -0500

    fix spawnpoint mixin

--- common/src/main/java/net/hecco/bountifulfares/mixin/gameplay/ServerPlayerEntityMixin.java
+++ common/src/main/java/net/hecco/bountifulfares/mixin/gameplay/ServerPlayerEntityMixin.java
@@ -7,17 +7,32 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayer.class)
 public class ServerPlayerEntityMixin {
+    @Shadow
+    private BlockPos respawnPosition;
+    @Shadow
+    private ResourceKey<Level> respawnDimension;
+    @Shadow
+    private float respawnAngle;
+    @Shadow
+    private boolean respawnForced;
 
     @Inject(method = "setRespawnPosition", at = @At("HEAD"), cancellable = true)
     private void bountifulfares$preventCustomBedSpawnPoint(ResourceKey<Level> dimension, @Nullable BlockPos pos, float angle, boolean forced, boolean sendMessage, CallbackInfo ci) {
-        if (((ServerPlayer)(Object)this).level().getBlockState(pos).is(BFBlocks.COIR_BED.get())) {
-            ci.cancel();
+        if (pos != null) {
+            if (((ServerPlayer) (Object) this).level().getBlockState(pos).is(BFBlocks.COIR_BED.get())) {
+                this.respawnPosition = null;
+                this.respawnDimension = Level.OVERWORLD;
+                this.respawnAngle = 0.0F;
+                this.respawnForced = false;
+                ci.cancel();
+            }
         }
     }
 }
