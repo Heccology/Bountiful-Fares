4af0e9212a9b0c9c2e7341c4b699dcef4bca00a7
Yirmiri <68178497+Yirmiri@users.noreply.github.com>
2025-08-13 17:56:12 -0400

    dispense

--- common/src/main/java/net/hecco/bountifulfares/registry/util/BFRegistries.java
+++ common/src/main/java/net/hecco/bountifulfares/registry/util/BFRegistries.java
@@ -11,6 +11,7 @@ import net.hecco.bountifulfares.registry.tags.BFItemTags;
 import net.hecco.heccolib.platform.HLServices;
 import net.hecco.heccolib.platform.services.HLRegistryHelper;
 import net.minecraft.core.Position;
+import net.minecraft.core.dispenser.BlockSource;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.ItemStack;
@@ -24,19 +25,21 @@ import static net.minecraft.world.level.block.ComposterBlock.COMPOSTABLES;
 
 public class BFRegistries {
     public static void registerModStuffs() {
-//        registerStrippables();
         registerCeramicCheckeredConversions();
 //        registerFuels();
         registerModCompostables();
+        registerDispenerBehavior();
+    }
 
+    public static void registerDispenerBehavior() {
         DispenserBlock.registerBehavior(BFItems.FLOUR.get(), new FlourDispenserBehavior() {});
 
-//        DispenserBlock.registerBehavior(BFItems.GRASS_SEEDS.get(), new GrassSeedsDispenserBehavior() {
-//            @Override
-//            public ItemStack execute(BlockSource pointer, ItemStack stack) {
-//                return super.execute(pointer, stack);
-//            } TODO
-//        });
+        DispenserBlock.registerBehavior(BFItems.GRASS_SEEDS.get(), new GrassSeedsDispenserBehavior() {
+            @Override
+            public ItemStack execute(BlockSource pointer, ItemStack stack) {
+                return super.execute(pointer, stack);
+            }
+        });
     }
 
     public static void registerCeramicCheckeredConversions() {
