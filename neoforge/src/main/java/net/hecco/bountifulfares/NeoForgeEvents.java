e7daf69380577e3ccc5fa907714fcdf842267cfe
Hecco <126524389+Hecco56@users.noreply.github.com>
2025-08-24 16:36:29 -0500

    changliog,,,,.,.,,.

--- neoforge/src/main/java/net/hecco/bountifulfares/NeoForgeEvents.java
+++ neoforge/src/main/java/net/hecco/bountifulfares/NeoForgeEvents.java
@@ -13,7 +13,6 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
