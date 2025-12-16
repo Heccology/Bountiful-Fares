b69f50f3a35c0ec77db2db6856802aff9653e7bf
DiemondPlayer <diemondplay@gmail.com>
2025-12-16 23:36:07 +0300

    a better way to implement ThrownPotionMixin

--- common/src/main/java/net/hecco/bountifulfares/mixin/gameplay/ThrownPotionMixin.java
+++ common/src/main/java/net/hecco/bountifulfares/mixin/gameplay/ThrownPotionMixin.java
@@ -1,10 +1,12 @@
 package net.hecco.bountifulfares.mixin.gameplay;
 
-import net.hecco.bountifulfares.definition.block.custom.ChamomileCandleBlock;
 import net.hecco.bountifulfares.definition.block.custom.CoconutCandleBlock;
 import net.hecco.bountifulfares.definition.block.custom.InfusedCandleBlock;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.entity.projectile.ThrownPotion;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,17 +15,20 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ThrownPotion.class)
-public abstract class ThrownPotionMixin extends EntityMixin {
+public abstract class ThrownPotionMixin extends ThrowableItemProjectile {
+
+    public ThrownPotionMixin(EntityType<? extends ThrowableItemProjectile> entityType, Level level) {
+        super(entityType, level);
+    }
+
     @Inject(method = "dowseFire", at = @At("TAIL"))
-    private void addFrontiersChecks(BlockPos pos, CallbackInfo ci)
-    {
+    private void bountifulfares$dowseFire(BlockPos pos, CallbackInfo ci) {
         BlockState blockState = this.level().getBlockState(pos);
         Block block = blockState.getBlock();
 
         if (block instanceof InfusedCandleBlock && blockState.getValue(InfusedCandleBlock.LIT)) {
             InfusedCandleBlock.extinguish(null, blockState, this.level(), pos);
-        }
-        else if (block instanceof CoconutCandleBlock && blockState.getValue(CoconutCandleBlock.LIT)) {
+        } else if (block instanceof CoconutCandleBlock && blockState.getValue(CoconutCandleBlock.LIT)) {
             CoconutCandleBlock.extinguish(null, blockState, this.level(), pos);
         }
     }
