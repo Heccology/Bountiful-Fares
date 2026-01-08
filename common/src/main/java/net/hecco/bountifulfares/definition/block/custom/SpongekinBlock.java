1d0ea66c413ae9fdec88cc1ab1507068e4be5881
Artyrian <77861508+TheArtyrian@users.noreply.github.com>
2026-01-07 23:52:05 -0500

    client/server spongekin sync

--- common/src/main/java/net/hecco/bountifulfares/definition/block/custom/SpongekinBlock.java
+++ common/src/main/java/net/hecco/bountifulfares/definition/block/custom/SpongekinBlock.java
@@ -5,7 +5,9 @@ import net.hecco.bountifulfares.registry.content.BFSounds;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.BlockParticleOption;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.ItemInteractionResult;
@@ -29,13 +31,27 @@ public class SpongekinBlock extends Block {
     protected ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit)
     {
         if (stack.is(Items.SHEARS) || stack.is(ItemTags.AXES)) {
-            for (int i = 0; i < 32 + world.random.nextIntBetweenInclusive(0, 16); i++) {
-                world.addParticle(new BlockParticleOption(ParticleTypes.BLOCK, BFBlocks.SPONGEKIN.get().defaultBlockState()), (pos.getX() - 0.2) + (world.random.nextFloat() * 1.4), pos.getY() + (world.random.nextFloat() * 1.2), (pos.getZ() - 0.2) + (world.random.nextFloat() * 1.4), (world.random.nextFloat() - 0.5) / 8, (world.random.nextFloat() - 0.5) / 8, (world.random.nextFloat() - 0.5) / 8);
+            if (!world.isClientSide()) {
+                player.awardStat(Stats.ITEM_USED.get(stack.getItem()));
+                BlockParticleOption spong = new BlockParticleOption(ParticleTypes.BLOCK, BFBlocks.SPONGEKIN.get().defaultBlockState());
+                for (int i = 0; i < 32 + world.random.nextIntBetweenInclusive(0, 16); i++) {
+                    ((ServerLevel)world).sendParticles(
+                            spong,
+                            (pos.getX() - 0.2) + (world.random.nextFloat() * 1.4),
+                            pos.getY() + (world.random.nextFloat() * 1.2),
+                            (pos.getZ() - 0.2) + (world.random.nextFloat() * 1.4),
+                            1,
+                            0.0F,
+                            0.0F,
+                            0.0F,
+                            0.0F
+                    );
+                }
             }
             world.setBlock(pos, Blocks.WET_SPONGE.defaultBlockState(), 2);
             player.getItemInHand(hand).hurtAndBreak(1, player, LivingEntity.getSlotForHand(hand));
             world.playSound(player, player.getX(), player.getY(), player.getZ(), BFSounds.SPONGEKIN_SHEAR.get(), SoundSource.BLOCKS, 1.0F, 0.8f + world.random.nextFloat()/4);
-            return ItemInteractionResult.SUCCESS;
+            return ItemInteractionResult.sidedSuccess(world.isClientSide);
         }
         return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
     }
