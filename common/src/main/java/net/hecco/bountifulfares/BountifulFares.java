c857840e264aaf95c3bfad054da421bdd788c1a9
Hecco <126524389+Hecco56@users.noreply.github.com>
2026-06-02 21:32:55 -0500

    start 3.0.11, walnut leaves retexture

--- common/src/main/java/net/hecco/bountifulfares/BountifulFares.java
+++ common/src/main/java/net/hecco/bountifulfares/BountifulFares.java
@@ -30,7 +30,6 @@ public class BountifulFares {
 	public static final String DELICATE_DYES_MOD_ID = "delicate_dyes";
     public static final String EVERY_COMPAT_MOD_ID = "everycomp";
 	public static final String DUNGEONSDELIGHT_MOD_ID = "dungeonsdelight";
-	public static final String URBAN_DECOR_MOD_ID = "urban_decor";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
